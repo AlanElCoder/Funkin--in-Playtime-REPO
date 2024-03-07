095bbf5c4acca8bdde8910cc5bdfbcb858ae82a8
ManuelCrack483 <manuelcrack483@gmail.com>
2024-03-06 23:56:58 -0300

    complaciendo al alan

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -94,8 +94,8 @@ class CreditsState extends MusicBeatState
 
 		pisspoop= [ //Name - Icon name - Description - Link - BG Color
 			['FUNKIN DREAM TEAM'],
-			['gabo','gabo','Concept Artist and Owner','https://twitter.com/gabro1209','5F5F5F'],
-			['AlanCoder.hx','alansin','Main Coder','https://twitter.com/Alanelcoderwe_l','FF0000'],
+			['gabo','gabo','Director and Concept Artist','https://twitter.com/gabro1209','5F5F5F'],
+			['AlanCoder.hx','alansin','Director and Main Coder','https://twitter.com/Alanelcoderwe_l','FF0000'],
 			['ManuelCrack483','les recuerdo q zari se olvido de mi nombre -manu','Coder - Hey muy buenas a todos guapisimos','https://twitter.com/miku_real','9AFF9F'],
 			['ZariDoodles','zari','Main Artist and Main Animator','https://x.com/Zari_DoodlesUvU?t=2esZEjwsOawICn6TR4OgQw&s=09','FFE77C'],
 			['Double L','double_l','Artist','https://twitter.com/FunniDoubleL','30F6FF'],
